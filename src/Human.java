dca3de6ae87c375a7ab457aed7f13b99ee8e0d2c
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2022-11-07 17:58:45 +0300

    Домашняя работа от 7 ноября 2022

--- src/Human.java
+++ src/Human.java
@@ -6,72 +6,11 @@ public class Human {
     //Учитывая использование шаблона проектирования "строитель", проверка и инициализация полей объекта Human
     //должно происходить только там
 
-    String none = "!!! Информация не указана !!!";
+    String none = "*** Информация не указана ***";
     private int yearOfBirth = 0;
     private String name = none;
     private String town = none;
     private String jobTitle = none;
-
-
-    //Внутренний статический (для использования в методе main при создании объектов Human) класс
-    //для создания объекта типа Human
-    public static class HumanBuilder {
-        String none = "!!! Информация не указана !!!";
-        private int yearOfBirth = 0;
-        private String name = none;
-        private String town = none;
-        private String jobTitle = none;
-
-        private int getYearOfBirth() {
-            return yearOfBirth;
-        }
-
-        private String getName() {
-            return name;
-        }
-
-        private String getTown() {
-            return town;
-        }
-
-        private String getJobTitle() {
-            return jobTitle;
-        }
-
-        public HumanBuilder setYearOfBirth(int yearOfBirth) {
-            if (yearOfBirth > 0) {
-                this.yearOfBirth = yearOfBirth;
-            }
-            return this;
-        }
-
-        public HumanBuilder setName(String name) {
-            if (name != null || !name.isEmpty()) {
-                this.name = name;
-            }
-            return this;
-        }
-
-        public HumanBuilder setTown(String town) {
-            if (town != null || !town.isEmpty()) {
-                this.town = town;
-            }
-            return this;
-        }
-
-        public HumanBuilder setJobTitle(String jobTitle) {
-            if (jobTitle != null || !jobTitle.isEmpty()) {
-                this.jobTitle = jobTitle;
-            }
-            return this;
-        }
-
-        public Human build() {
-            return new Human(this);
-        }
-    }
-
-    //-----------------------------------------------------------------------------------------------
 
     //Комментарий целесообразности использования паттерна Builder
     //При создании объекта пользователь может не задать какой-либо из параметров
@@ -93,16 +32,12 @@ public class Human {
     // город + должность
     // должность
     // ...
-    //Многовато...
+    //... Многовато ...
     //К тому же неудобно помнить очередность указания полей при создании объекта
-    //Поэтому и использую паттерн проектирования Builder
-    //
+    //Поэтому и использую паттерн проектирования Builder для создания объекта Human
+    //Информацию по паттерну "нагуглил", если избыточное решение, то удалю.
 
-
-    //Конструктор БЕЗ параметров - для первой части задания
-    public Human() {
-    }
-
+    //Конструктор с использованием объекта HumanBuilder
     public Human(HumanBuilder human) {
         this.name = human.getName();
         this.town = human.getTown();
@@ -110,34 +45,56 @@ public class Human {
         this.yearOfBirth = human.getYearOfBirth();
     }
 
-    //Конструктор с указанием значений всех полей - для выполнения условия задачи
-    public Human(int yearOfBirth, String name, String town, String jobTitle) {
-        if (checkInputString(name)) {this.name = name;}
-        if (checkInputString(town)) {this.town = town;}
-        if (checkInputString(jobTitle)) {this.jobTitle = jobTitle;}
-        if (yearOfBirth < 0) {this.yearOfBirth = 0;}
+    //Конструктор БЕЗ параметров - для первой части задания
+    public Human() {
     }
 
+    //Конструктор с указанием значений всех полей - для выполнения условия задачи
+    public Human(int yearOfBirth, String name, String town, String jobTitle) {
+        if (checkInputString(name)) {
+            this.name = name;
+        }
+        if (checkInputString(town)) {
+            this.town = town;
+        }
+        if (checkInputString(jobTitle)) {
+            this.jobTitle = jobTitle;
+        }
+        if (yearOfBirth > 0) {
+            this.yearOfBirth = yearOfBirth;
+        }
+    }
 
-    public boolean checkInputString(String string) {
-        return string != null || !string.isEmpty();
+    //Вспомогательный метод для проверки вводимых текстовых значений полей
+    //Использую этот метод, т.к. поля "имя", "город", "должность" - текстовые, имеют один тип
+    //Делаю проверку по длине вводимого текста:
+    //Если string.length() == 0 (например значение поля указано ""), то результат false
+    //Если длина строки больше нуля, то какое-то значение указано
+    public boolean checkInputString (String string) {
+        return string.length() > 0;
     }
 
     public void setYearOfBirth(int yearOfBirth) {
-        this.yearOfBirth = yearOfBirth;
+        if (yearOfBirth > 0) {this.yearOfBirth = yearOfBirth;}
     }
+
     public void setName(String name) {
-        this.name = name;
+        if (checkInputString (name)) {
+            this.name = name;
+        }
     }
 
     public void setTown(String town) {
-        this.town = town;
+        if (checkInputString (town)) {
+            this.town = town;
+        }
     }
 
     public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
+        if (checkInputString (jobTitle)) {
+            this.jobTitle = jobTitle;
+        }
     }
-
 
     @Override
     public String toString() {
@@ -147,5 +104,68 @@ public class Human {
                 + " Я работаю на должности " + jobTitle
                 + ". Будем знакомы!";
 
+    }
+
+    //-----------------------------------------------------------------------------------------------
+    //Внутренний статический (для использования в методе main при создании объектов Human) класс
+    //для создания объекта типа Human
+    public static class HumanBuilder {
+        String none = "*** Информация не указана ***";
+        private int yearOfBirth = 0;
+        private String name = none;
+        private String town = none;
+        private String jobTitle = none;
+
+        private int getYearOfBirth() {
+            return yearOfBirth;
+        }
+
+        private String getName() {
+            return name;
+        }
+
+        private String getTown() {
+            return town;
+        }
+
+        private String getJobTitle() {
+            return jobTitle;
+        }
+
+        public boolean checkInputString (String string) {
+            return string.length() > 0;
+        }
+
+        public HumanBuilder setYearOfBirth(int yearOfBirth) {
+            if (yearOfBirth > 0) {
+                this.yearOfBirth = yearOfBirth;
+            }
+            return this;
+        }
+
+        public HumanBuilder setName(String name) {
+            if (checkInputString(name)) {
+                this.name = name;
+            }
+            return this;
+        }
+
+        public HumanBuilder setTown(String town) {
+            if (checkInputString(town)) {
+                this.town = town;
+            }
+            return this;
+        }
+
+        public HumanBuilder setJobTitle(String jobTitle) {
+            if (checkInputString(jobTitle)) {
+                this.jobTitle = jobTitle;
+            }
+            return this;
+        }
+
+        public Human build() {
+            return new Human(this);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
-        //Задания по классу "Human"
+       //Задания по классу "Human"
 
-        /*Human Максим = new Human.HumanBuilder().setName("A").setTown("City").setYearOfBirth(-3).build();
-        System.out.println(Максим.toString());*/
-        System.out.println("Результат выполнения заданий:");
+        System.out.println("Результат выполнения заданий по классу \"Human\":");
         //Создание объектов Human без конструктора
+        System.out.println("Объекты класса Human, значения полей которых заданы НЕ через конструктор:");
         Human Максим = new Human();
-        Максим.setName("");
+        Максим.setName("Максим");
         Максим.setTown("Минск");
         Максим.setJobTitle("бренд-менеджер");
         Максим.setYearOfBirth(1988);
@@ -34,36 +32,113 @@ public class Main {
         Артем.setJobTitle("директор по развитию бизнеса");
         Артем.setYearOfBirth(1995);
         System.out.println(Артем);
+        System.out.println();
 
-        //Создание объектов Human через конструктор
-       /* Human human_1 = new Human(1988, "Максим", "Минск", "бренд-менеджер");
-        System.out.println(human_1.toString());
+        //Передача параметров полей ранее созданным объектам Human через конструктор
+        System.out.println("Объекты класса Human, значения полей которых заданы через конструктор:");
+        Максим = new Human(1981, "Максим", "Гродно", "менеджер");
+        System.out.println(Максим);
 
-        Human human_2 = new Human(1993, "Аня", "Москва", "методист образовательных программ");
-        System.out.println(human_2.toString());
+        Аня = new Human(1998, "Аня", "Москва", "заместитель генерального директора");
+        System.out.println(Аня);
 
-        Human human_3 = new Human(1992, "Катя", "Калининград", "продакт-менеджер");
-        System.out.println(human_3.toString());
+        Катя = new Human(1993, "Катя", "Владивосток", "инструктор");
+        System.out.println(Катя);
 
-        Human human_4 = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
-        System.out.println(human_4.toString());
+        Артем = new Human(1996, "Артем", "Москва", "директор холдинга");
+        System.out.println(Артем);
 
         System.out.println();
 
-        //Задание 3
-        System.out.println("Результат выполнения задания 3:");
-        Car ladaGranta = new Car("Lada", "Granta", 2015, "Россия", "желтый", 1.7);
-        System.out.println(ladaGranta.toString());
-        Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "черный", 3.0);
-        System.out.println(audiA8.toString());
-        Car bmwZ8= new Car("BMW", "Z8", 2021, "Германия", "черный", 3.0);
-        System.out.println(bmwZ8.toString());
+        //Создадим объект Human с пустыми полями через конструктор
+        System.out.println("Создадим объект Human с пустыми полями через конструктор:");
+        Human testHuman_1 = new Human(-100, "Василий", "", "");
+        System.out.println(testHuman_1);
+        System.out.println();
 
-        Car kiaSportage = new Car("Kia", "Sportage 4-го поколения", 2018, "Южная Корея", "красный", 2.4);
-        System.out.println(kiaSportage.toString());
-        Car hyundaiAvante = new Car("Hyundai", "Avante", 2016, "Южная Корея", "оранжевый", 1.6);
-        System.out.println(hyundaiAvante.toString());*/
+        //Создадим объект Human используя builder
+        System.out.println("Создадим объект Human используя builder:");
+        Human testHuman_2 = new Human.HumanBuilder().setName("Андрей").setYearOfBirth(2000).build();
+        Human testHuman_3 = new Human.HumanBuilder().setTown("Tokyo").build();
+        Human testHuman_4 = new Human.HumanBuilder().build();
+        //Используя проще указывать верные значения полей
+        //И нет жесткой привязки к конструктору, если не все поля можем ввести
+        System.out.println(testHuman_2);
+        System.out.println(testHuman_3);
+        System.out.println(testHuman_4);
 
+        System.out.println();
+        System.out.println("----------------------------------------------------------------------");
+
+        //Задания по классу "Car"
+        System.out.println("Результат выполнения задания по классу \"Car\":");
+        System.out.println();
+        //Создание объектов Car, не передавая значения через конструктор
+        System.out.println("Создание объектов Car, не передавая значения через конструктор:");
+        Car ladaGranta = new Car();
+        ladaGranta.setModel("Lada");
+        ladaGranta.setBrand("Granta");
+        ladaGranta.setProductionCountry("Россия");
+        ladaGranta.setColor("желтый");
+        ladaGranta.setEngineVolume(1.7);
+        ladaGranta.setProductionYear(2015);
+        System.out.println(ladaGranta);
+
+        Car audiA8 = new Car();
+        audiA8.setModel("Audi");
+        audiA8.setBrand("A8 50 L TDI quattro");
+        audiA8.setProductionCountry("Германия");
+        audiA8.setColor("черный");
+        audiA8.setEngineVolume(3.0);
+        audiA8.setProductionYear(2020);
+        System.out.println(audiA8);
+
+        Car bmwZ8 = new Car();
+        bmwZ8.setModel("BMW");
+        bmwZ8.setBrand("Z8");
+        bmwZ8.setProductionCountry("Германия");
+        bmwZ8.setColor("черный");
+        bmwZ8.setEngineVolume(3.0);
+        bmwZ8.setProductionYear(2021);
+        System.out.println(bmwZ8);
+
+        Car kiaSportage = new Car();
+        kiaSportage.setModel("Kia");
+        kiaSportage.setBrand("Sportage 4-го поколения");
+        kiaSportage.setProductionCountry("Южная Корея");
+        kiaSportage.setColor("красный");
+        kiaSportage.setEngineVolume(2.4);
+        kiaSportage.setProductionYear(2018);
+        System.out.println(kiaSportage);
+
+
+        Car hyundaiAvante = new Car();
+        hyundaiAvante.setModel("Hyundai");
+        hyundaiAvante.setBrand("Avante");
+        hyundaiAvante.setProductionCountry("Южная Корея");
+        hyundaiAvante.setColor("оранжевый");
+        hyundaiAvante.setEngineVolume(1.6);
+        hyundaiAvante.setProductionYear(2016);
+        System.out.println(hyundaiAvante);
+        System.out.println();
+
+        //Заполнение полей объектов Car, передавая их значения через конструктор
+        System.out.println("Создание объектов Car, передавая значения через конструктор:");
+        ladaGranta = new Car("Lada", "Granta", 2017, "Россия", "баклажан", 1.7);
+        System.out.println(ladaGranta);
+        audiA8 = new Car("Audi", "A8 50 L TDI quattro", 2021, "Германия", "черный", 3.0);
+        System.out.println(audiA8);
+        bmwZ8 = new Car("BMW", "Z8", 2022, "Германия", "синий", 3.0);
+        System.out.println(bmwZ8);
+        kiaSportage = new Car("Kia", "Sportage 4-го поколения", 2019, "Южная Корея", "зеленый", 2.4);
+        System.out.println(kiaSportage);
+        hyundaiAvante = new Car("Hyundai", "Avante", 2018, "Южная Корея", "оранжевый", 1.6);
+        System.out.println(hyundaiAvante);
+        System.out.println();
+        //Создадим объект Car с пустыми полями
+        System.out.println("Создадим объект Car с пустыми полями:");
+        Car testCar = new Car("AAAA", "", 0, "", "", -6);
+        System.out.println(testCar);
 
     }
 }
